aaaade4a34d7eed796da13a9c9005bf8e7288ea9
fri tol <4304520+fritol@users.noreply.github.com>
2024-04-17 14:23:29 +0200

    Update plot.py color maps option
    plot_single() was resulting in odd dark low contrast plots for me
    
    the mod includes `cmap` parameter

--- src/uni2ts/eval_util/plot.py
+++ src/uni2ts/eval_util/plot.py
@@ -17,26 +17,29 @@ def plot_single(
     dim: Optional[int] = None,
     name: Optional[str] = None,
     show_label: bool = False,
+    cmap: str = 'viridis' # Add a new parameter for the color map
 ):
     ax = maybe.unwrap_or_else(ax, plt.gca)
+    print('A plotsingle')
 
     target = np.concatenate([inp["target"], label["target"]], axis=-1)
     start = inp["start"]
     if dim is not None:
         target = target[dim]
         forecast = forecast.copy_dim(dim)
-
+    print('B plotsingle')
     index = pd.period_range(start, periods=len(target), freq=start.freq)
     ax.plot(
         index.to_timestamp()[-context_length - forecast.prediction_length :],
         target[-context_length - forecast.prediction_length :],
         label="target",
-        color="black",
+        color=cm.get_cmap(cmap)(0.5), # Use the color map for the plot color
     )
+    print('C plotsingle')
     forecast.plot(
         intervals=intervals,
         ax=ax,
-        color="blue",
+        color=cm.get_cmap(cmap)(0.75), # Use the color map for the forecast color
         name=name,
         show_label=show_label,
     )
